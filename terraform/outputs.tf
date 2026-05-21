4a598f1eb31719a1955e3ced1e83186a12e113ab
TengkuRizal <rizal.devsecops@gmail.com>
2026-05-21 19:43:57 +0800

    Fix Container App public endpoint output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,5 +3,5 @@ output "acr_login_server" {
 }
 
 output "app_url" {
-  value = "https://${azurerm_container_app.main.latest_revision_fqdn}"
+  value = "https://${azurerm_container_app.main.ingress[0].fqdn}"
 }
